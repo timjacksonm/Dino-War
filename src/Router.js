807ca63ec776bbfdc0d25dc69034e52a6d99569f
Tim Jackson <76879054+timjacksonm@users.noreply.github.com>
2021-08-01 16:04:25 -0500

    Revert "Feature/game logic"

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,12 @@
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Level from './components/Level';
 
 const Router = () => {
   return (
     <HashRouter>
       <Switch>
-        <Route exact path="/">
+        <Route path="/">
           <Home />
-        </Route>
-        <Route exact path="/levels/beginner/:stage">
-          <Level />
-        </Route>
-        <Route exact path="/levels/average/:stage">
-          <Level />
-        </Route>
-        <Route exact path="/levels/hard/:stage">
-          <Level />
         </Route>
       </Switch>
     </HashRouter>
